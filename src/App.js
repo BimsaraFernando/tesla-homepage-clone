f3002d8fd8259d933feff6090fe5501a15c2c633
BimsaraFernando <kavindu.16@itfac.mrt.ac.lk>
2021-01-23 15:18:17 +0530

    header+assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi KABI</h1>
+      <Header />
+      {/* snapping items  */}
     </div>
   );
 }
